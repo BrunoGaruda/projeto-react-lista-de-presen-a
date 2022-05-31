bec53acf0389de236090dc92be0aba901b5c0f0e
BrunoGaruda <bruleorn@gmail.com>
2022-05-31 20:45:09 -0300

    cleanapp

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 
+//Renderizando o componente "app" dentro do id root na div html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
