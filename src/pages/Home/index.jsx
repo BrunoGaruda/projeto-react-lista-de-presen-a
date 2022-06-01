9cbcb12b2316a2ae6554e76b111fa12f12e90a14
BrunoGaruda <bruleorn@gmail.com>
2022-06-01 03:10:33 -0300

    Princípio da imutabilidade, adicionando novos estudantes ao clicar adicionar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,49 @@
+import React, { useState } from 'react' // Necessário a (hook = useState) importação para refletir a "let" na interface
 import './styles.css'
 
 import { Card } from '../../components/Card'
 
 //Numa estrutura jsx você tem uma função que retorna o conteúdo HTML pra ser renderizado
 export function Home() {
+  const [studentName, setstudentName] = useState()
+  const [students, setStudents] = useState([])
+
+  //Vai ser criado um novo objeto adicionado o nome pegando do estado no condeúdo do  input e o time no horário atual "toLocaleDateString"
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleDateString('pt-br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    }
+    //Com essa função você consegue recuperar o antigo studend e adicinando um novo ao vetor
+
+    //... serve para evitar que o prevState crie um novo vetor, despejando todos em um mesmo vetor.
+    setStudents(prevState => [...prevState, newStudent])
+  }
+
+  function handleNameChange(name) {
+    studentName = name
+  }
   //Tag JSX precisa ser "embrulhada" para nao dar erro. <> </> ou uma <div>
   return (
     <div className="container">
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite o nome..." />
-      <button type="button">Adicionar</button>
+      <h1>Lista de Presença</h1>
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        //Pegando o conteúdo atual do input e atualizando o estado com setstudentName
+        onChange={e => setstudentName(e.target.value)}
+      />
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
 
-      <Card name="Bruno" time="10:55:25" />
-      <Card name="Karol" time="11:22:18" />
+      {students.map(student => (
+        <Card name={student.name} time={student.time} />
+      ))}
     </div>
   )
 }
