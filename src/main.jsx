6237566d39b066030ca3290bde665737fc80e304
BrunoGaruda <bruleorn@gmail.com>
2022-06-01 01:54:50 -0300

    criação do primeiro component Card

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/global.css'
 //Home tem q ser maiúsculo pra funcionar no Import
-import Home from './pages/Home'
+import { Home } from './pages/Home'
 
 //Renderizando o componente "app" dentro do id root na div html
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,17 @@
+import './styles.css'
+
+import { Card } from '../../components/Card'
+
 //Numa estrutura jsx você tem uma função que retorna o conteúdo HTML pra ser renderizado
-function Home() {
+export function Home() {
   //Tag JSX precisa ser "embrulhada" para nao dar erro. <> </> ou uma <div>
   return (
-    <div>
+    <div className="container">
       <h1>Lista de presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
+
+      <Card />
     </div>
   )
 }
-
-export default Home
